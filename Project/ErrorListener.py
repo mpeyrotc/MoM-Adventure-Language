a1d719cd783b94dc662864e0d7838f4d42f70b72
Marco Peyrot <mpeyrotc@dhcp-10-16-177-163.mty.itesm.mx>
2017-09-22 22:20:52 -0500

    Add Simple ErrorListener messages
    Summary:
    The messages are simple enough to help
    us find bugs in the grammar.
    
    The main method reads from the command line and prints
    'OK' if the program is good or an error message if not.

--- Project/ErrorListener.py
+++ Project/ErrorListener.py
@@ -6,13 +6,13 @@ class MoMErrorListener(ErrorListener):
         super(MoMErrorListener, self).__init__()
 
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        raise Exception("Oh no!!")
+        raise Exception("Oh no! " + str(offendingSymbol) + " ++++++ " + msg)
 
     def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
         raise Exception("Oh no!!")
 
     def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
-        raise Exception("Oh no!!")
+        raise Exception("Oh no!!! " + str(dfa))
 
     def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
-        raise Exception("Oh no!!")
+        raise Exception("Oh no!!!!")
